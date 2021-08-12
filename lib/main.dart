7d37c8cb087415a126269a8c380d126058c70afb
ronaldemanuel <emronald15el@gmail.com>
2021-08-11 22:30:04 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TheJournalApp());
+}
+
+class TheJournalApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Journal',
+      home: Scaffold(
+        body: Text('Hello World!'),
+      ),
+    );
+  }
+}
