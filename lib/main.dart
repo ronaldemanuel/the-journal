fbf29c128ffc21e9cdc2c4ac41b943fa42c3922e
Ronald Emanuel <emronald15el@gmail.com>
2021-10-06 00:26:14 +0000

    parte de baixo finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class TheJournalApp extends StatelessWidget {
     return MaterialApp(
       title: 'The Journal',
       home: Scaffold(
-        body: Text('Tela incial'),
+        body: Text('Fiz a parte de baixo'),
       ),
     );
   }
